bebb591fea794cceda302e35fc0f4ca2e8447e80
gim159 <gim159@naver.com>
2021-04-01 15:17:14 +0900

    21-04-01 환유율 수정

--- hms_project/src/main/java/com/hotel/hms/controller/CommuteController.java
+++
@@ -1,9 +0,0 @@
-package com.hotel.hms.controller;
-
-import org.springframework.stereotype.Controller;
-
-// 근태 컨트롤러
-@Controller
-public class CommuteController {
-
-}
--- hms_project/src/main/java/com/hotel/hms/service/WelfareService.java
+++
@@ -1,5 +0,0 @@
-package com.hotel.hms.service;
-
-public interface WelfareService {
-
-}
